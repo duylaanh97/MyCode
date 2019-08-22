ba376cfdd811be2eb85c578e9f4e14d8ee583cc3
duylaanh97 <45973983+duylaanh97@users.noreply.github.com>
2019-08-22 14:58:39 +0700

    code

---
+++ WebAngular/blog/src/app/configuration.ts
@@ -0,0 +1,9 @@
+export const configuration = {
+    header: {
+    heading: 'Leezy Blog',
+    headingtext: 'create blog, write blog, everything you feel, you think, come for me',
+    buttontext: 'Start Now',
+    buttonlink: '\home'
+    },
+
+  };
